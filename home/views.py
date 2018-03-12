cc3c407a6225bc2d2eb62b1b96bb6351db091b8d
Stephen Moody <stephengmoody@hotmail.com>
2018-03-12 10:18:29 +0000

    Changes Made2

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from messenger.models import Message
 
 def get_index(request):
-    unread = request.user.messages_receied.filter(read=False)
+    unread = request.user.messages_received.filter(read=False)
     return render(request, "home/index.html", {"unread" : len(unread)})
 
 
